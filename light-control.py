d5360d3f2076b75f49845d6fc219329ddf415ab8
rahulgs12 <rahul@Rahuls-MacBook-Pro.local>
2017-09-25 20:23:38 -0400

    light turn off func

--- light-control.py
+++ light-control.py
@@ -23,3 +23,22 @@ def change(status, whichLights):
                 finalStr+= hex(ord(i))
             finalStr+="0D"
             ser.write(finalStr)
+    else:
+        for light in whichLights:
+            tempHex = hex(light)
+            if (tempHex.length == 1):
+                tempHex = "0" + tempHex;
+            chksum = bin((62 + light) % 256)
+            newsum = ""
+            for i in list(checksum):
+                if i=="1":
+                    newsum += "1"
+                else:
+                    newsum += "0"
+            cSum = hex(1+int(newsum,2))
+            fullStr = ("\\05380001" + tempHex + cSum).upper()
+            finalStr = ""
+            for i in list(fullStr):
+                finalStr+= hex(ord(i))
+            finalStr+="0D"
+            ser.write(finalStr)
