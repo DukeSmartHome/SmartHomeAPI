c5ecb1706f7a7f25ef0f20ce74809d2473d076dc
rahulgs12 <rahul@Rahuls-MacBook-Pro.local>
2017-09-26 22:37:11 -0400

    pyt

--- light-control.py
+++ light-control.py
@@ -1,60 +1,53 @@
-#
-# import serial
-# PORT = ""
-# ser = serial.Serial(PORT)
+import time
+
+import serial
+PORT = "/dev/cu.lpss-serial1"
+ser = serial.Serial(PORT)
 
 lights = [["Dirty Lab", "26", "labs"], ["Clean Lab Cabinets", "3", "labs"], ["Clean Lab", "4", "labs"], ["South West Bedroom", "6", "living"], ["Downstairs Bedroom", "7", "living"], ["North West Bedroom", "9", "living"], ["North East Bedroom", "21", "living"], ["South East Bedroom", "28", "living"], ["West Balcony", "35", "outside"], ["Front Porch", "35", "outside"], ["Back Porch", "36", "outside"], ["Kitchen", "11", "community"], ["Front Indoor Lights", "12", "community"], ["White Board Lights", "31", "community"], ["Kitchen Cabinets", "38", "community"], ["Main Room", "11", "community"], ["Media Room", "20", "community"], ["Upper Floor", "0", "community"], ["East Upper Bathroom", "17", "bathrooms"], ["West Upper Bathroom", "15", "bathrooms"], ["West Lower Bathroom", "13", "bathrooms"]];
 
+settings = dict()
+settings["ON"] = ["182","\\05380079"]
+settings["OFF"] = ["62","\\05380001"]
+
+
+
 def change(status, whichLights):
-    if status == "ON":
-        for light in whichLights:
-            tempHex = light
-            if (len(str(tempHex)) == 1):
-                tempHex = "0" + tempHex;
-            chksum = bin(int("182" + str(light))%256).replace("0b","")
-            newsum = ""
-            print()
-            for i in list(str(chksum)):
-                if i=="1":
+    if (status != "ON"):
+        status = "OFF"
+    for light in whichLights:
+        tempHex = light
+        if (len(str(light)) == 1):
+            tempHex = "0" + tempHex
+        chksum = bin((int(settings[status][0]) + int(light)) % 256 ).replace("0b","")
+        newsum = ""
+        for i in list(str(chksum)):
+            if i=="1":
+                newsum += "0"
+            else:
+                newsum += "1"
+        cSum = hex(1+int(newsum,2)).replace("0x","")
+        fullStr = (settings[status][1]  + tempHex + cSum).upper()
+        finalStr = ""
+        for i in list(fullStr):
+            finalStr+=hex(ord(i[0])).replace("0x","")
+        # finalStr+="0D"
+        return finalStr
+#
+# # ans = "["
+# for i in range(200):
+#     for i in lights:
+#         change("OFF", i[1])
+#         print("ON")
+#         time.sleep(5)
+#
+#         change("ON", i[1])
+#         print("OFF")
+        # time.sleep(5)
 
-                    newsum += "0"
-                else:
-                    newsum += "1"
-            # return newsum
-            cSum = hex(1+int(newsum,2)).replace("0x","")
-            fullStr = ("\\05380079" + tempHex + cSum).upper()
-            finalStr = ""
-            for i in list(fullStr):
-                finalStr+=hex(ord(i[0])).replace("0x","")
-            finalStr+="0D"
-            return finalStr
-    else:
-        for light in whichLights:
-            # light=int(light)
-            tempHex = hex(light)
-            if (len(str(tempHex)) == 1):
-                tempHex = "0" + tempHex;
-            chksum = bin((62 + light) % 256)
-            newsum = ""
-            for i in list(chksum):
-                if i=="1":
-                    newsum += "1"
-                else:
-                    newsum += "0"
-            cSum = hex(1+int(newsum,2))
-            fullStr = ("\\05380001" + tempHex + cSum).upper()
-            finalStr = ""
-            for i in list(fullStr):
-                finalStr+= hex(ord(i))
-            finalStr+="0D"
-            return finalStr.replace("0x","")
 
-            # ser.write(finalStr)
-ans = "["
-for i in lights:
-    ans+="\""+str(change("ON",i[1]))+"\""
-    ans+=","
-    # ans+="\""+change("OFF",i[1])+"\""
-    # ans+=","
-ans+="]"
-print(ans)
+
+
+
+print(change("ON", ["26"]).encode())
+ser.write(change("OFF", ["26"]).encode())
