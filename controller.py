6add5db8fbc5dcde7b8e7a6e87aa07788dc61709
Harvey Shi <suyash.kumar@duke.edu>
2017-10-14 17:55:26 -0400

    s

--- controller.py
+++ controller.py
@@ -6,13 +6,13 @@ config = firebase_config.config
 
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
-
+lights.change("ON",["26"])
 
 def stream_handler(message):
 
 	light = message["path"].replace("/","")
 	status = message["data"]
-	print("changing with",status,light)
+	print("changing with status ",status," as ",light)
 	if isinstance(light,int):
 		lights.change(str(status),[str(light)])
 
